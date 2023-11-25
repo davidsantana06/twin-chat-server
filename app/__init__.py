b9216f64f03111accf63fe7e0f8e44e1a66f0dbe
davidsantana06 <davidsantana06@hotmail.com>
2023-11-25 15:13:37 -0300

    feat: add cors to app

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from os import path
 
 from .constants import ROOT_FOLDER
@@ -7,18 +8,14 @@ from .routes import router
 
 
 def create_app() -> FastAPI:
-    load_dotenv(
-        path.join(ROOT_FOLDER, '.env')
-    )
+    load_dotenv(path.join(ROOT_FOLDER, '.env'))
 
     app = FastAPI(
         title='TwinChat API',
-        description=(
-            'TwinChat is a straightforward platform for providing ' + \
-            'answers to fitness-related questions in Portuguese (PT-BR).'
-        ),
+        description='TwinChat is a straightforward platform for providing answers to fitness-related questions in Portuguese (PT-BR).',
         version='0.1.0'
     )
+    app.add_middleware(CORSMiddleware, allow_origins=['*'])
     app.include_router(router)
 
     return app
